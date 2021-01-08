9c6cd153657aed2ec747d8fa96207f9bdc065c34
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-01-08 10:09:39 +0100

    SHOULD_USE_TEXT_CONTENT = /^(?:plaintext|script|style|textarea|title|xmp)$/i

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var DOCUMENT_FRAGMENT_NODE = 11;
 var ELEMENT_NODE = 1;
 var TEXT_NODE = 3;
 
-var SHOULD_USE_TEXT_CONTENT = /^(?:style|textarea)$/i;
+var SHOULD_USE_TEXT_CONTENT = /^(?:plaintext|script|style|textarea|title|xmp)$/i;
 var VOID_ELEMENTS = /^(?:area|base|br|col|embed|hr|img|input|keygen|link|menuitem|meta|param|source|track|wbr)$/i;
 
 exports.UID = UID;
