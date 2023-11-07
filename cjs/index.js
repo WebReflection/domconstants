3a9d337f2fed17ca6dd897427c6e000583f59cad
webreflection <andrea.giammarchi@gmail.com>
2023-11-07 09:24:41 +0100

    Exported also sets

--- cjs/index.js
+++ cjs/index.js
@@ -16,7 +16,41 @@ exports.DOCUMENT_TYPE_NODE = DOCUMENT_TYPE_NODE;
 const DOCUMENT_FRAGMENT_NODE = 11;
 exports.DOCUMENT_FRAGMENT_NODE = DOCUMENT_FRAGMENT_NODE;
 
-const TEXT_ELEMENTS = /^(?:plaintext|script|style|textarea|title|xmp)$/i;
+/** @type {Set<'PLAINTEXT' | 'SCRIPT' | 'STYLE' | 'TEXTAREA' | 'TITLE' | 'XMP'>} */
+const TEXT_SET = new Set([
+  'PLAINTEXT',
+  'SCRIPT',
+  'STYLE',
+  'TEXTAREA',
+  'TITLE',
+  'XMP',
+]);
+exports.TEXT_SET = TEXT_SET;
+
+/** @type {Set<'AREA' | 'BASE' | 'BR' | 'COL' | 'EMBED' | 'HR' | 'IMG' | 'INPUT' | 'KEYGEN' | 'LINK' | 'MENUITEM' | 'META' | 'PARAM' | 'SOURCE' | 'TRACK' | 'WBR'>} */
+const VOID_SET = new Set([
+  'AREA',
+  'BASE',
+  'BR',
+  'COL',
+  'EMBED',
+  'HR',
+  'IMG',
+  'INPUT',
+  'KEYGEN',
+  'LINK',
+  'MENUITEM',
+  'META',
+  'PARAM',
+  'SOURCE',
+  'TRACK',
+  'WBR',
+]);
+exports.VOID_SET = VOID_SET;
+
+const re = set => new RegExp(`^(?:${[...set].join('|')})$`, 'i');
+
+const TEXT_ELEMENTS = re(TEXT_SET);
 exports.TEXT_ELEMENTS = TEXT_ELEMENTS;
-const VOID_ELEMENTS = /^(?:area|base|br|col|embed|hr|img|input|keygen|link|menuitem|meta|param|source|track|wbr)$/i;
+const VOID_ELEMENTS = re(VOID_SET);
 exports.VOID_ELEMENTS = VOID_ELEMENTS;
--- esm/index.js
+++ esm/index.js
@@ -8,5 +8,37 @@ export const DOCUMENT_NODE = 9;
 export const DOCUMENT_TYPE_NODE = 10;
 export const DOCUMENT_FRAGMENT_NODE = 11;
 
-export const TEXT_ELEMENTS = /^(?:plaintext|script|style|textarea|title|xmp)$/i;
-export const VOID_ELEMENTS = /^(?:area|base|br|col|embed|hr|img|input|keygen|link|menuitem|meta|param|source|track|wbr)$/i;
+/** @type {Set<'PLAINTEXT' | 'SCRIPT' | 'STYLE' | 'TEXTAREA' | 'TITLE' | 'XMP'>} */
+export const TEXT_SET = new Set([
+  'PLAINTEXT',
+  'SCRIPT',
+  'STYLE',
+  'TEXTAREA',
+  'TITLE',
+  'XMP',
+]);
+
+/** @type {Set<'AREA' | 'BASE' | 'BR' | 'COL' | 'EMBED' | 'HR' | 'IMG' | 'INPUT' | 'KEYGEN' | 'LINK' | 'MENUITEM' | 'META' | 'PARAM' | 'SOURCE' | 'TRACK' | 'WBR'>} */
+export const VOID_SET = new Set([
+  'AREA',
+  'BASE',
+  'BR',
+  'COL',
+  'EMBED',
+  'HR',
+  'IMG',
+  'INPUT',
+  'KEYGEN',
+  'LINK',
+  'MENUITEM',
+  'META',
+  'PARAM',
+  'SOURCE',
+  'TRACK',
+  'WBR',
+]);
+
+const re = set => new RegExp(`^(?:${[...set].join('|')})$`, 'i');
+
+export const TEXT_ELEMENTS = re(TEXT_SET);
+export const VOID_ELEMENTS = re(VOID_SET);
